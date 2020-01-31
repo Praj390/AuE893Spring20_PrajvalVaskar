eed9630a77318830faf73415da2f9fd5d6c269a8
Prajval Vaskar <pvaskar@g.clemson.edu>
2020-01-30 21:25:32 -0500

    turtlesim assignment 2

---
+++ assignment2_ws/build/assignment_2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/prajval/AuE8930/git_ws/AuE893Spring20_PrajvalVaskar/assignment2_ws/src/assignment_2
+# Build directory: /home/prajval/AuE8930/git_ws/AuE893Spring20_PrajvalVaskar/assignment2_ws/build/assignment_2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
